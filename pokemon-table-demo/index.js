d959eed2ff3a6e2c2584e85c041eada95a598c9d
Bipul Chaudhary <bipulchau@gmail.com>
2022-07-17 16:26:08 -0400

    Fetch API using async await function

--- pokemon-table-demo/index.js
+++ pokemon-table-demo/index.js
@@ -1,6 +1,8 @@
 let pokemonIdInput = document.querySelector("#pokemon-id");
 let thenCatchButton = document.querySelector("#then-catch-btn");
 thenCatchButton.addEventListener("click", thenCatchDemo);
+let asyncAwaitButton = document.querySelector("#async-await-btn");
+asyncAwaitButton.addEventListener("click", asyncAwaitDemo);
 
 function thenCatchDemo() {
   fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonIdInput.value}`)
@@ -8,11 +10,27 @@ function thenCatchDemo() {
       return res.json(); // .json returns another promise that contains the response body converted into a JS object
     })
     .then((data) => {
+      console.log(".then .catch");
       addPokemonToTable(data);
+    })
+    .catch((err) => {
+      console.log(err);
     });
 }
 
-function asyncAwaitDemo() {}
+async function asyncAwaitDemo() {
+  try {
+    let res = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/${pokemonIdInput.value}`
+    );
+
+    let data = await res.json();
+    console.log("Kappu");
+    addPokemonToTable(data);
+  } catch (err) {
+    console.log(err);
+  }
+}
 
 function addPokemonToTable(pokemon) {
   let pokemonTbodyElement = document.querySelector("#poke-table tbody");
